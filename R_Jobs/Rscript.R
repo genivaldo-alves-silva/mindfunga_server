e73155713b42ed01088920e1ca856c89019f64d5
Genivaldo Alves da Silva <33955162+genivaldo-alves-silva@users.noreply.github.com>
2026-03-17 18:28:01 +0000

    Update documentation

---
+++ R_Jobs/Rscript.R
@@ -0,0 +1,15 @@
+
+library(Rcpp)
+library(dada2)
+library(stringr)
+library(ggplot2)
+library(here)
+library(ShortRead)
+library(Biostrings)
+library(tibble)
+library(dplyr)
+
+#setwd('/home/genis/16S_')
+
+list.files()
+
